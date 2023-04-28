052df6b45d8b7c6a6e1f1775fd50c6fcbf33f46b
Finlay Hlannon <79950262+finlayhlannon@users.noreply.github.com>
2023-04-27 21:04:15 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var usedCodes = [
+  "example", "finlay", "hayes", "aspect", "bhavesh", "death", "bunny", "far", "jedi princess chelsea", "melo", "minions", "nnn", "random mortis", "samriddhi", "shivnotech", "unimmortalpanda", "zain", "grxim", "nxtviv", "pepsi", "rishii", "marv", "mjrn"
+];
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var input = document.querySelector('.search-input');
+  var id = input.value.trim().toLowerCase();
+  var url = 'file:///C:/Users/finla/OneDrive/Documents/Code/ID/badges/' + id + '.html';
+  
+  // check if the code is in the usedCodes array
+  if (usedCodes.includes(id)) {
+    window.location.href = url;
+  } else {
+    alert('Invalid code!');
+  }
+}
+
+var submitButton = document.querySelector('.search-button');
+submitButton.addEventListener('click', handleSubmit);
+
+var inputField = document.querySelector('.search-input');
+inputField.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    handleSubmit(event);
+  }
+});
